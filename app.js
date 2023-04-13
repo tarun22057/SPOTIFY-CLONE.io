b9f308256b8ea33d56d4a8ee1af1bb05daf765f9
Tarun Singh <tarunsingh22057@gmail.com>
2023-04-14 02:18:07 +0530

    implemented search bar

--- app.js
+++ app.js
@@ -67,42 +67,93 @@ const search = async(selectedOption, query) => {
             configSearches
         );
         // console.log(response);
-        return response.data[selectedOption + "s"].items; // this "s" appended because the api returns json obejct as tracks : .... and we are passing selectedOption = track,album etc so we need to append s
+        // return response.data[selectedOption + "s"].items; // this "s" appended because the api returns json obejct as tracks : .... and we are passing selectedOption = track,album etc so we need to append s
+        return response.data.tracks.items;
     } catch (err) {
         console.log("couldn't find what you were looking for");
     }
 };
 
-const select = document.querySelector("#select-options");
-const searchBar = document.querySelector(".search");
+// const select = document.querySelector("#select-options");
+const searchBar = document.querySelector(".search-bar");
+let creatingXinSearch = 0; //this is to create that X in search bar when the user types a single word it appears in the search bar
 
-select.addEventListener("change", async function() {
-    const selectedOption = this.value;
-    const query = searchBar.value; //gets the value from the serach bar
+searchBar.addEventListener("input", async function() {
+    // const selectedOption = this.value; if you want to search for albums etc use the select option in the html
+    const selectedOption = "track";
+    const query = this.value; //gets the value from the serach bar
     // console.log(query);
     // console.log(selectedOption);
+
+    creatingXinSearch++;
+    console.log(creatingXinSearch);
+    //only once the button should be appned to the
+    const crossButton = document.createElement("button");
+    crossButton.innerText = "X";
+    crossButton.classList.add("cross-button");
+    const searchBarFa = document.querySelector(".search-bar-fa");
+    if (creatingXinSearch === 1) {
+        searchBarFa.appendChild(crossButton);
+        creatingXinSearch = 999;
+        console.log(creatingXinSearch);
+    }
+
+    const cross = document.querySelector(".cross-button");
+    cross.addEventListener("click", function() {
+        searchBar.value = "";
+        // crossButton.innerText = "";
+    });
+
     try {
         const searchResults = await search(selectedOption, query);
         console.log(searchResults);
 
         const resultsContainer = document.querySelector(".results-container");
-        // Clear the search results container
 
+        // Clear the search results container
+        resultsContainer.innerHTML = "";
         for (let i = 0; i < 10; i++) {
             const result = document.createElement("div");
-            result.classList.add("result"); //  dynamically adding class names to div
+            result.classList.add("result-div"); //  dynamically adding class names to div
+
+            //create a div tag and give numbering
+            const resultNumber = document.createElement("div");
+            resultNumber.textContent = i + 1;
+            result.appendChild(resultNumber);
+            resultNumber.classList.add("result-number"); //  dynamically adding class names to span tags
 
             // create an image element for the search result
             const resultImg = document.createElement("img");
             resultImg.src = searchResults[i].album.images[0].url;
             result.appendChild(resultImg);
-            resultImg.classList.add("searchedImg"); //  dynamically adding class names to img
+            resultImg.classList.add("searched-img"); //  dynamically adding class names to img
 
-            // create a paragraph element for the search result name
-            const resultName = document.createElement("p");
+            // create a div element for the search result name
+            const resultName = document.createElement("div");
             resultName.textContent = searchResults[i].name;
             result.appendChild(resultName);
-            resultName.classList.add("searchedSongName"); //  dynamically adding class names to p tags
+
+            //create a div element for the search result artist
+            const resultArtist = document.createElement("div");
+            resultArtist.textContent = searchResults[i].artists[0].name;
+            resultName.appendChild(resultArtist);
+            resultName.classList.add("searched-song-name"); //  dynamically adding class names to p tags
+
+            //get the album to which this song belongs
+            const resultAlbum = document.createElement("div");
+            resultAlbum.textContent = searchResults[i].album.name;
+            result.appendChild(resultAlbum);
+            resultAlbum.classList.add("searched-song-album"); //  dynamically adding class names to p tags
+
+            //create a div tag for song timing
+            const songTime = document.createElement("div");
+
+            const time_ms = searchResults[i].duration_ms; // gets time in milliseconds
+            const time_sec = millisToMinutesAndSeconds(time_ms);
+            songTime.textContent = time_sec;
+
+            result.appendChild(songTime);
+            songTime.classList.add("searched-song-time"); //  dynamically adding class names to span tags
 
             // append the search result to the results container
             resultsContainer.appendChild(result);
@@ -112,6 +163,11 @@ select.addEventListener("change", async function() {
     }
 });
 
+function millisToMinutesAndSeconds(millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+}
 //unauthorized to get the email and user detail
 
 // const checkPremium = async() => {
@@ -134,3 +190,7 @@ select.addEventListener("change", async function() {
 //         console.error(error);
 //     }
 // };
+
+// --> improve the search bar that is if you change a word the list changes and add a cancel button to close the list
+// --> start buildind the UI (recently played , playlist liked songs etc ...)
+// -->
